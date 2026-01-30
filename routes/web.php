fe27a1079c22de0c6757bdd504322696d2188ad0
a-oirgari <abdrahmanghali1998@gmail.com>
2026-01-30 23:35:55 +0100

    chore: the main routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,32 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PublicController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+Route::get('/', [PublicController::class, 'home'])->name('home');
+
+
+Route::get('/categories', [PublicController::class, 'categories'])->name('public.categories');
+
+
+Route::get('/categorie/{slug}', [PublicController::class, 'categoryProducts'])->name('public.category.products');
+
+
+Route::get('/produit/{id}', [PublicController::class, 'productDetail'])->name('public.product.detail');
+
+
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    
+    
+    Route::resource('categories', CategoryController::class);
+    
+    
+    Route::resource('products', ProductController::class);
 });
